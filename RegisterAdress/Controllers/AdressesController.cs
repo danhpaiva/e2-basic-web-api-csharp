fd07ee49b96a32e4cb4dba24c1c3e330d8bed66d
Paiva <danpaiva@live.com>
2020-11-09 21:30:23 -0300

    First Commit

---
+++ RegisterAdress/Controllers/AdressesController.cs
@@ -0,0 +1,31 @@
+﻿using RegisterAdress.Models;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace RegisterAdress.Controllers
+{
+    public class AdressesController : ApiController
+    {
+        private static List<Adress> adresses = new List<Adress>();
+
+        [HttpGet]
+        public List<Adress> GetList()
+        {
+            return adresses;
+        }
+
+        [HttpPost]
+        public void PostAdress(string street, string number, string district, string city, string uf)
+        {
+            if ((!string.IsNullOrEmpty(street)) && (!string.IsNullOrEmpty(number)) && (!string.IsNullOrEmpty(district)) && (!string.IsNullOrEmpty(city)) && (!string.IsNullOrEmpty(uf)))
+                adresses.Add(new Adress(street, number, district, city, uf));
+        }
+
+        [HttpDelete]
+        public void DeleteAdress(string street)
+        {
+            adresses.RemoveAt(adresses.IndexOf(adresses.First(x => x.Street.Equals(street))));
+        }
+    }
+}
---
+++ RegisterAdress/Models/Adress.cs
@@ -0,0 +1,20 @@
+﻿namespace RegisterAdress.Models
+{
+    public class Adress
+    {
+        public string Street { get; set; }
+        public string Number { get; set; }
+        public string District { get; set; }
+        public string City { get; set; }
+        public string UF { get; set; }
+
+        public Adress(string street, string number, string district, string city, string uf)
+        {
+            this.Street = street;
+            this.Number = number;
+            this.District = district;
+            this.City = city;
+            this.UF = uf;
+        }
+    }
+}
